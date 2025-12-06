082b0d53b2c5cf0b3f98c777f841a3b644aa7a2b
Akash Das <akashakki1100@gmail.com>
2025-12-06 21:03:16 +0530

    hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className=" dark:bg-black relative">
       < Navbar theme={theme} setTheme={setTheme} />
+      <Hero/>
     </div>
   );
 }
